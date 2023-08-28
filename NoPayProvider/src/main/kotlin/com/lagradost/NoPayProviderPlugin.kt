112c39a5e319f6f116cb34ce20eca1bf9cbd4c84
antonycaporossi <antony.caporossi@gmail.com>
2023-08-28 17:00:32 +0200

    Fix

--- NoPayProvider/src/main/kotlin/com/lagradost/NoPayProviderPlugin.kt
+++ NoPayProvider/src/main/kotlin/com/lagradost/NoPayProviderPlugin.kt
@@ -1,11 +1,11 @@
-package com.example
+package com.lagradost
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 
 @CloudstreamPlugin
-class NoPayPlugin: Plugin() {
+class NoPayProviderPlugin: Plugin() {
     override fun load(context: Context) {
         // All providers should be added in this manner. Please don't edit the providers list directly.
         registerMainAPI(NoPayProvider())
